8d114c0f22d35795f78358ac3ea713dad4d76a8f
MarinLovrinovic <lovrinovic.marin@gmail.com>
2025-06-01 06:41:50 +0200

    Printer done

--- gamejam_31.5.2025/scripts/printer.gd
+++ gamejam_31.5.2025/scripts/printer.gd
@@ -6,6 +6,8 @@ extends Node3D
 @export var printed_object: PackedScene
 
 var player_inside = false
+var printed
+var printing_finished = false
 
 func _ready():
 	password_popup.initiate(self)
@@ -19,14 +21,31 @@ func _on_interact_area_body_exited(body: Node3D) -> void:
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		if !player_inside: return
-		password_popup.interaction_attempt()
+		
+		if printing_finished:
+			print("Upgrade!!")
+			if printed:
+				printed.visible = false
+		else:
+			password_popup.interaction_attempt()
 				
 func text_submitted(text):
 	if text == password:
 		print("Correct")
-		var printed = printed_object.instantiate()
-		printed.position.y += 0.6
+		printed = printed_object.instantiate()
+		printed.position.y += 0.5
 		add_child(printed)
+		var print_mat = load("res://materials/printing_mat.tres");
+		printed.material_override = print_mat;
+		var limit : float = 1.3;
+		while (limit > -0.2):
+			var delta = get_process_delta_time();
+			limit -= delta / 2;
+			(printed.get_active_material(0) as ShaderMaterial).set_shader_parameter("limit", limit);
+			await get_tree().process_frame;
+		printed.material_override = null;
+		printing_finished = true
+				
 	else:
 		print("Incorrect")
 		

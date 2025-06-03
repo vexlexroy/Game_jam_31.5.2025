09080701f4a99517b5d2ba8bc057e9460505b0b5
Leonardo Roy Sabolić <148395425+vexlexroy@users.noreply.github.com>
2025-06-03 21:50:37 +0200

    some fixes(collision/sens)

--- gamejam_31.5.2025/scripts/player_camera.gd
+++ gamejam_31.5.2025/scripts/player_camera.gd
@@ -11,6 +11,7 @@ class_name PlayerCamera
 ## Get transform on start
 @onready var start_rotation = self.rotation;
 @onready var parent_start_rotation = rotation_parent.rotation if rotation_parent != null else null;
+@export var mouse_sensitivity = 0.0025
 
 func reset_rotation():
 	if (rotation_parent != null):
@@ -24,21 +25,21 @@ func look_input(relative):
 			0: # First person
 				# Vertical
 				if (rotate_vertical):
-					self.rotate_x(-relative.y * 0.01);
+					self.rotate_x(-relative.y * mouse_sensitivity);
 					self.rotation.x = clamp(self.rotation.x, deg_to_rad(-80), deg_to_rad(80))
 				# Horizontal
-				rotation_parent.rotate_y(-relative.x * 0.01);
+				rotation_parent.rotate_y(-relative.x * mouse_sensitivity);
 			1: # Third person
 				# Vertical
 				if (rotate_vertical):
-					$"..".rotate_x(relative.y * 0.01);
+					$"..".rotate_x(relative.y * mouse_sensitivity);
 					$"..".rotation.x = clamp($"..".rotation.x, deg_to_rad(-20), deg_to_rad(20))
 				# Horizontal
-				rotation_parent.rotate_y(-relative.x * 0.01);
-				#rotation_parent.global_rotate(Vector3.RIGHT, -relative.y * 0.01);
-				#rotation_parent.global_rotate(Vector3.UP, -relative.x * 0.01);
+				rotation_parent.rotate_y(-relative.x * mouse_sensitivity);
+				#rotation_parent.global_rotate(Vector3.RIGHT, -relative.y * mouse_sensitivity);
+				#rotation_parent.global_rotate(Vector3.UP, -relative.x * mouse_sensitivity);
 			2: # Top down
-				rotation_parent.rotate_y(-relative.x * 0.01);
+				rotation_parent.rotate_y(-relative.x * mouse_sensitivity);
 				pass
 
 func get_forward():
